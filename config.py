c1ed4c8a0124fc2a767a6b3af446c0f568eca5ac
SONU KANWAR <111267360+Priya0178@users.noreply.github.com>
2024-07-13 13:35:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 API_ID = int(os.environ.get("API_ID", "19397648"))
 API_HASH = os.environ.get("API_HASH", "ed2db4aa6ab5f67fde7b88b8f17e85d0")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5464339478:AAExMDU2ChelgnqW0P2WsuVxlWe6JBo76iY")
-DB_CHANNEL_ID = os.environ.get("DB_CHANNEL_ID", "-1001773229209")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6882113180:AAFNON75dwmZ6LgeLT2R2YaffDvSSMoBrys")
+DB_CHANNEL_ID = os.environ.get("DB_CHANNEL_ID", "-1002150772901")
 IS_PRIVATE = os.environ.get("IS_PRIVATE", True) # any input is ok But True preferable
 OWNER_ID = int(os.environ.get("OWNER_ID", "1092802988"))
 UPDATE_CHANNEL = os.environ.get('UPDATE_CHANNEL', '')
